f6cd51c5ef2feadd0ebf998a8989c82ebea2b3a0
PizzaBurger99 <alex.lh.000@gmail.com>
2019-04-27 00:18:13 +0200

    Added a temporary image - made a manu you can open and close with some example squares

---
+++ functions.js
@@ -0,0 +1,9 @@
+
+function ToggleMenu(){
+    var submenu = document.getElementById("submenu");
+    if(submenu.style.visibility=="hidden"){
+        submenu.style.visibility="";
+    }else{
+        submenu.style.visibility="hidden";
+    }
+}
